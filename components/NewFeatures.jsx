e3e4d576b212dee0f2000cabc061114aa012f0d0
Saul Paredes <saulkurosaki13@gmail.com>
2023-09-02 19:31:25 -0600

    NewFeature.jsx Refactor Done

--- components/NewFeatures.jsx
+++ components/NewFeatures.jsx
@@ -1,9 +1,9 @@
-import styles from "../styles";
+import styles from '../styles';
 
 const NewFeatures = ({ imgUrl, title, subtitle }) => (
   <div className="flex-1 flex flex-col sm:max-w-[250px] min-w-[210px]">
     <div className={`${styles.flexCenter} w-[70px] h-[70px] rounded-[24px] bg-[#323f5d]`}>
-      <img 
+      <img
         src={imgUrl}
         alt="Icon"
         className="w-1/2 h-1/2 object-contain"
